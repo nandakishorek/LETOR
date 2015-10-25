3b8c47cf3752f7bb11383d1e8269421bc03c6218
Nandakishore K <nandakishorek17@gmail.com>
2015-10-25 14:42:05 -0400

    changed cluster variance calculation, set model complexity to 26

--- proj2_real.m
+++ proj2_real.m
@@ -1,3 +1,4 @@
+function [] = proj2_real()
 %% Project 2 - Learning to Rank using Linear Regression
 %% Nandakishore Krishna
 %% Person number : 50169797
@@ -15,20 +16,26 @@ load('querylevelnorm.mat');
 % training set - 80% of dataset
 trainingX = Querylevelnorm(1:55700, 2:end);
 trainingT = Querylevelnorm(1:55700, 1);
+trainInd1 = 1:55700;
+trainInd1 = trainInd1';
 
 % validation set - 10 % dataset
-validationX = Querylevelnorm(55700:62661, 2:end);
-validationT = Querylevelnorm(55700:62661, 1);
+validationX = Querylevelnorm(55701:62661, 2:end);
+validationT = Querylevelnorm(55701:62661, 1);
+validInd1 = 55701:62661;
+validInd1 = validInd1';
 
 % testing set - 10% of dataset
 testingX = Querylevelnorm(62662:end, 2:end);
 testingT = Querylevelnorm(62662:end, 1);
+testInd1 = 62662:size(Querylevelnorm,1);
+testInd1 = testInd1';
 
 % number of training samples
-n = size(trainingX, 1);
+n1 = size(trainingX, 1);
 
 % no. of dimensions of the training set
-d = size(trainingX, 2);
+d1 = size(trainingX, 2);
 
 % histograms for the dataset
 % figure(1)
@@ -37,7 +44,7 @@ d = size(trainingX, 2);
 % histogram(trainingX(:,2));
 
 % model complexity
-M1 = 44;
+M1 = 26;
 
 % find the clusters for the datapoints
 fprintf('Finding %d clusters ...\n', M1 );
@@ -49,48 +56,77 @@ mu1 = C;
 
 % spread for the Gaussian radial functions
 fprintf('Calculating the spread for the %d Gaussian radial functions ...\n', M1);
-cluster_variance = zeros(M1,1);
+cluster_variance = zeros(M1,d1);
 for i = 1 : M1
-%     temp = [];
-%     for j = 1 : length(idx)
-%         if j == i
-%             temp = [temp; trainingX(j,:)];
-%         end
-%     end
-%     cluster_variance(i,1) = var(temp);
-    cluster_variance(i,1) = 1;
+    temp = [];
+    for j = 1 : length(idx)
+        if j == i
+            temp = [temp; trainingX(j,:)];
+        end
+    end
+    cluster_variance(i,:) = var(temp);
 end
 
 % the sigmaj for the basis functions
-Sigma1 = zeros(d,d,M1);
-
-% determine design matrix N X M
-fprintf('Calculating the design matrix phi of size %d X %d ...\n', n, M1);
-phi = ones(n, M1);
+Sigma1 = zeros(d1,d1,M1);
 for j = 2 : M1
-    Sigma1(:,:,j) = cluster_variance(j)' * eye(d);
-    siginv = inv(Sigma1(:,:,j));
-    for i = 1 : n
-        temp = trainingX(i,:)' - mu1(j);
-        phi(i,j) = exp(-1 / 2 * (temp' * siginv * temp) );
+    for i = 1 : n1
+        Sigma1(:,:,j) = diag(cluster_variance(j,:));
     end
 end
 
+% determine design matrix N X M
+Phi1 = calculatePhi(trainingX, M1, Sigma1, mu1);
+
 % regularization coefficient
-lambda = 0;
+lambda1 = 0;
 
 % closed form solution for the weights
 fprintf('Finding the closed form solution ...\n');
-w1 = pinv((lambda * eye(M1)) + phi' * phi) * phi' * trainingT;
+w1 = pinv((lambda1 * eye(M1)) + Phi1' * Phi1) * Phi1' * trainingT;
 
-% sum of squares error for training set
-error1 = (sum((trainingT - (phi * w1)) .^ 2) / 2) + (lambda * w1' * w1 / 2)
+% sum of squares error and erms for the training set
+[errorTest1, trainPer1] = calculateError(Phi1, trainingT, w1, size(trainingX, 1), lambda1)
 
-% root mean square error for training set
-trainPer1 = sqrt(2 * error1 / n)
+% validation set
+phiValid = calculatePhi(validationX, M1, Sigma1, mu1);
+[errorVal1, validPer1] = calculateError(phiValid, validationT, w1, size(validationX, 1), lambda1)
 
-y = phi * w1;
+% testing set
+phiTest = calculatePhi(testingX, M1, Sigma1, mu1);
+[errorTest1, testPer2] = calculateError(phiTest, testingT, w1, size(testingX, 1), lambda1);
+
+y = Phi1 * w1;
 xaxis = linspace(0, length(y), length(y));
 plot(xaxis, trainingT, 'g', xaxis, y, 'r');
 
 save('proj2_real.mat');
+end
+
+function Phi = calculatePhi(X, M, Sigma, mu)
+
+% number of training samples
+n = size(X, 1);
+
+% determine design matrix N X M
+fprintf('Calculating the design matrix phi of size %d X %d ...\n', n, M);
+Phi = ones(n, M);
+for j = 2 : M
+    for i = 1 : n
+        temp = X(i,:)' - mu(j);
+        siginv = inv(Sigma(:,:,j));
+        Phi(i,j) = exp(-1 * (temp' * siginv * temp) / 2);
+    end
+end
+
+end
+
+function [err, erms] = calculateError(phi, t, w, n, lambda)
+
+% sum of squares error
+err = sum((t - (phi * w)) .^ 2) / 2 + (lambda * (w' * w) / 2);
+
+% root mean square error
+erms = sqrt(2 * err / n);
+
+end
